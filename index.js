1b6a4b9041228c0dd08dac0815ef8a646d45dd62
jahpd <gcravista@gmail.com>
2017-06-02 08:26:03 -0300

    added first files

--- index.js
+++ index.js
@@ -3,29 +3,30 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = function(deps, core, done){
-    for(let p in deps){
-	let _p = path.join(__dirname, `boot/${deps[p]}.js`);
+module.exports = function(opt, done){
+    for(let i in opt.deps){
+	let _p = path.join(opt.path, `boot/${opt.deps[i]}.js`);
 	let str = ["\n\"use strict\""];
 
 	// First contextualize the code
 	// with common core library
-	for(let __pkg__ in core){
-	    str.push(`const ${name} = require('${__pkg__}')`);
+	for(let j in opt.core){
+	    let __pkg__ = opt.core[j]
+	    str.push(`const ${__pkg__} = require('${__pkg__}')`);
 	}
 
 	// Now the third pirth libraries
-	for(let __pkg__ in pkg[deps[p]]){
-	    let name = __pkg__.replace(/-/, "_")
+	let _dep = opt.pkg[opt.deps[i]];
+	for(let j in _dep){
+	    let name = j.replace(/-/, "_")
 	    if(!name.match(/grunt.*/) && !name.match(/check_node/)){
-		str.push(`const ${name} = require('${__pkg__}')`);
-		grunt.log.writeln(`${__pkg__} added to ${deps[p]}`)
+		str.push(`const ${name} = require('${j}')`);
 	    }
 	}
 	
 	fs.writeFile(_p, str.join(";\n\n"), function(err){
 	    if(err) done(err);
-	    if(p == 1) done();
+	    if(i == 1) done();
 	})
     }
 }
