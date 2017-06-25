3e869603ae0c5624d3fd8613e232c09eb54818b6
jahpd <gcravista@gmail.com>
2017-06-25 18:22:20 -0300

    added suport to js and coffeescript

--- index.js
+++ index.js
@@ -6,26 +6,44 @@ const path = require('path');
 module.exports = function(opt, done){
     let __deps__ = ["dependencies", "devDependencies"];
     for(let i in __deps__){
-	let _p = path.join(opt.path, `${opt.destination}/${__deps__[i]}.js`);
-	let str = ["\n\"use strict\""];
+	let _p = path.join(opt.path, `${opt.destination}/${__deps__[i]}.${opt.ext}`);
+	let str = []
+	if(opt.ext === 'js'){
+	    str.push("\n\"use strict\"");
+	}
 
 	// First contextualize the code
 	// with common core library
 	for(let j in opt.core){
 	    let __pkg__ = opt.core[j]
-	    str.push(`const ${__pkg__} = require('${__pkg__}')`);
+	    if(opt.ext === 'js'){
+		str.push(`const ${__pkg__} = require('${__pkg__}')`);
+	    }
+	    else if(opt.ext === 'coffee'){
+		str.push(`${__pkg__} = require '${__pkg__}'`);
+	    }
+	    else{
+		throw new Error("no extension defined");
+	    }
 	}
-
 	// Now the third pirth libraries
 	let _dep = opt.pkg[__deps__[i]];
 	for(let j in _dep){
 	    let name = j.replace(/-/g, "_")
 	    if(opt.validate(name)){
-		str.push(`const ${name} = require('${j}')`);
+		if(opt.ext === 'js'){
+		    str.push(`const ${name} = require('${j}')`);
+		}
+		if(opt.ext === 'coffee'){
+		    str.push(`${name} = require '${j}'`);
+		}
 	    }
 	}
-	
-	fs.writeFile(_p, str.join(";\n"), function(err){
+
+	let text = ""
+	if(opt.ext === 'js') text = str.join(";\n")
+	if(opt.ext === 'coffee') text = str.join("\n")
+	fs.writeFile(_p, text, function(err){
 	    if(err) done(err);
 	    if(i == 1) done();
 	})
